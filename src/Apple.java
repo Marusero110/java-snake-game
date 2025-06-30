1df718f04c3503d43eafc396feb455ac8aee8323
Marusero110 <marcelojgutierrez2001@gmail.com>
2025-06-29 20:10:15 -0400

    minor revision

--- src/Apple.java
+++ src/Apple.java
@@ -11,19 +11,17 @@ public class Apple {
         spawn(maxScreenColumns, maxScreenRows);
     }
 
-    // Spawn the apple at a random grid position.
     public void spawn(int maxScreenColumns, int maxScreenRows) {
         x = random.nextInt(maxScreenColumns);
         y = random.nextInt(maxScreenRows);
     }
 
-    // Draw the apple.
     public void draw(Graphics graphics, int tileSize) {
         graphics.setColor(Color.RED);
         graphics.fillOval(x * tileSize, y * tileSize, tileSize, tileSize);
     }
 
-    // Check if the snake eats the apple (if the snake's head is on the apple's position).
+    // Check if the snake ate the apple (if the snake's head is on the apple's position).
     public boolean isEaten(Snake snake) {
         int[] snakeHead = snake.getHead();
         return (snakeHead[0] == x) && (snakeHead[1] == y);
